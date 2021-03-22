c0a9ff59e6b7104438ef0124b4529c7b466cab79
AdvaOren1 <AdvaOren@carmel6000.amitnet.org>
2021-03-22 21:46:02 +0200

    add user details page

--- App.js
+++ App.js
@@ -11,32 +11,40 @@ import Location from './location'
 import MediumBlueButton from "./MediumBlueButton";
 import SmallBlueButton from "./SmallBlueButton";
 import YellowButton from "./YellowButton";
+import UserDetails from './userDetails';
+import { vh, vw } from 'react-native-expo-viewport-units';
+import profilImage from './assets/Portrait.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
-       {/* <YellowButton text="שלום לכולם" /> */}
-       {/* <LargeBlueButton text="הרשם" /> */}
-       {/* <FilterButton text="בוקר" onPress={()=>{}} changeBackground={true} /> */}
+      <UserDetails profilImage={profilImage} name='אדווה אורן' heroism={1399} nextTag={59} volunteerHours={37}/>
+      {/* <YellowButton text="שלום לכולם" /> */}
+      {/* <LargeBlueButton text="הרשם" /> */}
+      {/* <FilterButton text="בוקר" onPress={()=>{}} changeBackground={true} /> */}
       {/* <FilterButton text="צהריים " onPress={()=>{}} changeBackground={true} /> */}
-       {/* <SmallBlueButton text='יציאה ' /> */}
-       {/* <MediumBlueButton text='tk ,rao ' /> */}
+      {/* <SmallBlueButton text='יציאה ' /> */}
+      {/* <MediumBlueButton text='tk ,rao ' /> */}
       {/* <FacebookButton /> */}
       {/* <GoogleButton /> */}
       {/* <LoginButton /> */}
       {/* <CheckButton text={"מהבית"} svgSource={ss} hasCheck={true} /> */}
       {/* <Location text={"gzrv nvch,"} image={require('./assets/pic.jpg')} date={"רביעי 12.3"} numberOfMeters={300} number={230} numberOfMinutes={30} /> */}
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* <Text>Open up App.js to start working on your app!</Text> */}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    // position: 'absolute',
+    // width: vw(100),
+    // height: vh(100),
     flex: 1,
     // backgroundColor: 'blue',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // opacity: 0.5,
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });
 
---
+++ userDetails.jsx
@@ -0,0 +1,117 @@
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, Image, Platform } from 'react-native';
+import { vh, vw } from 'react-native-expo-viewport-units';
+// import styles from './fonts';
+import * as Font from 'expo-font';
+import getFontName from './fontGetName';
+
+export default function UserDetails(props) {
+    const [loaded, setLoaded] = useState(false)
+
+    useEffect(() => {
+        (async () => {
+            if (Platform.OS === 'ios') {
+                await Font.loadAsync({
+                    'rubik-bold': require('./assets/fonts/Rubik-Bold.ttf'),
+                })
+            } else {
+                await Font.loadAsync({
+                    'rubikBold': require('./assets/fonts/Rubik-Bold.ttf'),
+                })
+            }
+            setLoaded(true)
+        })()
+    }, [])
+    if (loaded) {
+        return (
+            <>
+                <View style={{...styles.container, backgroundColor: props.color ? props.color : '#fcfccc'}}>
+                    <Image source={props.profilImage} style={styles.personImage} />
+                    <Text style={styles.text}>{props.name} </Text>
+                    <View style={styles.details}>
+                        <View>
+                            <Text style={styles.data}>{props.heroism}</Text>
+                            <Text style={styles.mainText}>גבורות</Text>
+                        </View>
+                        <View style={styles.border}></View>
+                        <View>
+                            <Text style={styles.data}>{props.nextTag}%</Text>
+                            <Text style={styles.mainText}>לתג הבא</Text>
+                        </View>
+                        <View style={styles.border}></View>
+                        <View>
+                            <Text style={styles.data}>{props.volunteerHours}</Text>
+                            <Text style={styles.mainText}>שעות התנדבות</Text>
+                        </View>
+                    </View>
+                </View>
+                {/* <View style={styles.shadow}></View> */}
+            </>
+        )
+    } else {
+        return <Text>hii</Text>
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        zIndex: 10,
+        flex: 4/7,
+        flexDirection: 'column',
+        // borderBottom: 90,
+        borderBottomLeftRadius: 60,
+        borderBottomRightRadius: 60,
+        top: 0,
+        alignItems: 'center',
+        // borderBottomColor: '#dedddc',
+        borderWidth: vw(0.2),
+        // borderLeftColor: '#dedddc',
+        // borderRightColor: '#dedddc',
+        borderColor: '#dedddc',
+        // borderRadius: 100,
+        borderColor: '#ddd',
+        borderBottomWidth: 0,
+        shadowColor: '#000',
+        shadowOffset: { width: vw(100), height: vh(10) },
+        shadowOpacity: 0.8,
+        shadowRadius: 100,
+        elevation: 29,
+    },
+    personImage: {
+        width: vw(33),
+        height: vw(33),
+        top: vh(6),
+        marginBottom: vh(6),
+        borderRadius: 100
+    },
+    text: {
+        top: vh(2),
+        fontSize: vw(8),
+        color: 'black',
+        marginBottom: vh(4),
+        fontFamily: getFontName('rubik-bold'),
+    },
+    details: {
+        width: '100%',
+        top: vh(5),
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'flex-end'
+    },
+    mainText: {
+        fontSize: vw(4),
+        color: '#ababab',
+        textAlign: 'center',
+    },
+    data: {
+        fontSize: vw(8),
+        color: 'black',
+        textAlign: 'center',
+        fontFamily: getFontName('rubik-bold'),
+    },
+    border: {
+        width: vw(0.3),
+        height: vh(10),
+        backgroundColor: '#9c9b9a'
+    },
+});
